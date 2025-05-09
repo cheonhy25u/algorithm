1e2cd7bfd6d046fe592f23adcb27a41b43871dd0
cheonhy25u <kor01chs@gmail.com>
2025-05-09 11:28:26 +0900

    0x03 문제 풀이

--- barkingdog/0x03/BOJ/3273.cpp
+++ barkingdog/0x03/BOJ/3273.cpp
@@ -2,12 +2,25 @@
 using namespace std; 
 
 int main(){
-    int size; 
-    vector<int> arr;
-    int x; 
-    cin >> size; 
-    for (int i = 0; i < size; i ++) cin >> arr[i];
-    cin >> x;
+    int n, x, num;
 
-        
+    //입력받기 
+    cin >> n; 
+    vector<int> array(n) ;
+    for (int i = 0; i < n; i++){
+        cin >> array[i];
+    }
+    cin >> x; 
+
+    sort(array.begin(), array.end());
+    int i = 0, j = n-1, cnt = 0;
+    
+    while(i < j){
+        int sum = array[i]+array[j];
+        if(sum==x){
+            cnt++; i++, j--;
+        }else if (sum < x) i++;
+        else j--;
+    }
+    cout << cnt;
 }
